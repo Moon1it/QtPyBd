cbb21d54106cebff24d8484f1acf22a17af8eda8
Moon1it <viza0010@gmail.com>
2022-01-04 00:16:59 +0700

    Login to the page with a password from the database

--- WindowLoginPassword.py
+++ WindowLoginPassword.py
@@ -1,76 +1,99 @@
 import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMessageBox, QWidget, QPushButton, QApplication
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QLabel, QMessageBox, QWidget, QPushButton, QApplication
 from PyQt5.QtCore import QCoreApplication
 import psycopg2
 from WidgetsPy import select_all
 
-
-class window(QWidget):
+class Second(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.initWindow()
 
-        self.initUI()
+    def initWindow(self):
 
-    def initUI(self):
+        self.lbl = QLabel('Вы вошли!\nЛогин и пароль верны!', self)
+        self.lbl.setFont(QtGui.QFont('Arial', 25))
+        self.lbl.move(40,50)
+        #self.button.clicked.connect(self.ok)
+
+        self.setWindowTitle('Главная')
+        self.setGeometry(1000, 400, 520, 250)
+        self.show()
+
+    def ok(self):
+        print('close clicked')
+        self.close()
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.InitWindow()
+
+    def InitWindow(self):
+
+        self.lbl_login = QtWidgets.QLabel('Логин', self)
+        self.lbl_login.move(132,30)
+
+        self.lbl_password = QtWidgets.QLabel('Пароль', self)
+        self.lbl_password.move(130,90)
+
+        self.txt_login = QtWidgets.QTextEdit(self)
+        self.txt_login.move(92,50)
+        self.txt_login.resize(120,30)
+
+        self.txt_password = QtWidgets.QTextEdit(self)
+        self.txt_password.move(92,110)
+        self.txt_password.resize(120,30)
         
-        def setText_login():
-            text = txt_login.toPlainText()
-            return text
-        
-        def setText_password():
-            text = txt_password.toPlainText()
-            return text
-        
-        def Check_login_password():
-            connection = psycopg2.connect(user="postgres",
-                                    password="root",
-                                    host="127.0.0.1",
-                                    port="5432",
-                                    database="postgres")
-        
-            cursor = connection.cursor()
-            cursor.execute("SELECT * from login")                          
-            rows = cursor.fetchall()
-            
-            cursor.close()
-            connection.close()
-            ck = 0
-            for row in rows:
-                if row[0] == setText_login() and row[1] == setText_password():
-                    print('Yes')
-                    ck = 1
-            if ck == 0:
-                QMessageBox.critical(self, "Ошибка", "Неправильный логин или пароль!", QMessageBox.Ok)
-                        
-        lbl_login = QtWidgets.QLabel(self)
-        lbl_login.setText('Логин')
-        lbl_login.move(132,30)
 
-        lbl_password = QtWidgets.QLabel(self)
-        lbl_password.setText('Пароль')
-        lbl_password.move(130,90)
-
-        txt_login = QtWidgets.QTextEdit(self)
-        txt_login.move(92,50)
-        txt_login.resize(120,30)
-
-        txt_password = QtWidgets.QTextEdit(self)
-        txt_password.move(92,110)
-        txt_password.resize(120,30)
-
-        qbtn = QPushButton('Войти', self)
-        qbtn.clicked.connect(Check_login_password)
-        qbtn.resize(120,30)
-        qbtn.move(92, 155)
+        self.qbtn = QPushButton('Войти', self)
+        self.qbtn.clicked.connect(self.Check_login_password)
+        self.qbtn.resize(120,30)
+        self.qbtn.move(92, 155)
 
         self.setGeometry(1000, 400, 300, 250)
         self.setWindowTitle('Войти')
         self.show()
 
+    def ContinueSecond(self):
+        self.close()
+        self.next = Second()
+        #next.__init__()
+    
+    def setText_login(self):
+            text = self.txt_login.toPlainText()
+            return text
         
+    def setText_password(self):
+        text = self.txt_password.toPlainText()
+        return text
+        
+    def Check_login_password(self):
+        connection = psycopg2.connect(user="postgres",
+                                password="root",
+                                host="127.0.0.1",
+                                port="5432",
+                                database="postgres")
+    
+        cursor = connection.cursor()
+        cursor.execute("SELECT * from login")                          
+        rows = cursor.fetchall()
+        cursor.close()
+        connection.close()
+    
+        ck = 0
+        
+        for row in rows:
+            if row[0] == self.setText_login() and row[1] == self.setText_password():        
+                ck = 1
+                self.ContinueSecond()
+        if ck == 0:
+            QMessageBox.critical(self, "Ошибка", "Неправильный логин или пароль!", QMessageBox.Ok)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = window()
+    w = Window()
     sys.exit(app.exec_())
